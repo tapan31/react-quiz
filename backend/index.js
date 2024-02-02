b6456f3d9ba3c312703e18e7b5a37432d5c0f475
Tapan <tapansonak786@gmail.com>
2024-02-02 22:26:30 +0530

    vercel

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,17 @@ const router = jsonServer.router("../data/questions.json");
 const middlewares = jsonServer.defaults();
 const cors = require("cors");
 
+const corsOptions = {
+  origin: "https://react-quiz-three-beta.vercel.app/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
 const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
-server.use(cors());
+server.use(cors(corsOptions));
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
