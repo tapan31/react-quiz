4e40faf5c02b0fecdf5d3016f3758b4b04272de4
Tapan <tapansonak786@gmail.com>
2024-02-02 22:05:34 +0530

    CORS Fix

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("../data/questions.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
+server.use(cors());
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
